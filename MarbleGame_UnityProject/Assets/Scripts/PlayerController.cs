cae878a1ed9c5e859177157efeefcf79a8cc871b
Ru McPharlin <s220749@students.aie.edu.au>
2023-06-30 10:50:15 +0930

    Camera scroll zoom

--- MarbleGame_UnityProject/Assets/Scripts/PlayerController.cs
+++ MarbleGame_UnityProject/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Camera")]
-    [SerializeField] GameObject VC1;
+    [SerializeField] GameObject[] cameras;
+    [SerializeField] GameObject activeCam;
+
+    [SerializeField] bool canChangeCamera;
+    [SerializeField] float canChangeCameraTimer;
+    [SerializeField] float canChangeCameraTimeLimit;
+
+    [SerializeField] int cameraPriority1 = 1;
+    [SerializeField] int cameraPriority0 = 0;
+    [SerializeField] int activeCamIndex;
+    [SerializeField] float activeCamValue; 
 
     [Space]
     [Header("GameObjects")]
@@ -36,6 +47,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        cameras[activeCamIndex].GetComponent<CinemachineVirtualCamera>().Priority = 1;
+        activeCam = cameras[activeCamIndex];
     }
 
     // Update is called once per frame
@@ -43,19 +56,76 @@ public class PlayerController : MonoBehaviour
     {
         if(playerActive)
         {
-            
-            TiltBoard(GetNearestSide());
-
+            TiltBoard();
+            CameraControl();
         }
 
         WS = Input.GetAxis("WS");
         AD = Input.GetAxis("AD");
+        Debug.Log(Input.mouseScrollDelta.y);
     }
 
-    private void TiltBoard(GameObject nearestSide)
+    private void CameraControl()
     {
-        float cameraX = VC1.transform.position.x;
-        float cameraZ = VC1.transform.position.z;
+        //get active cam value
+        activeCamValue = cameras[activeCamIndex].GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineOrbitalTransposer>().m_XAxis.Value;
+
+        //set all values to be equal
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            if (i != activeCamIndex)
+            {
+                cameras[i].GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineOrbitalTransposer>().m_XAxis.Value = activeCamValue;
+            }
+        }
+        
+        //scroll up
+        if(Input.mouseScrollDelta.y < 0 && canChangeCamera && activeCamIndex < cameras.Length-1)
+        {
+            //change priority
+            cameras[activeCamIndex].GetComponent<CinemachineVirtualCamera>().Priority = cameraPriority0;
+            cameras[activeCamIndex+1].GetComponent<CinemachineVirtualCamera>().Priority = cameraPriority1;
+
+            //increase camera index
+            activeCamIndex++;
+
+            //set can change camera to false
+            canChangeCamera = false;
+            canChangeCameraTimer = canChangeCameraTimeLimit;
+        }
+
+        //scroll down
+        if (Input.mouseScrollDelta.y > 0 && canChangeCamera && activeCamIndex > 0)
+        {
+            //change priority
+            cameras[activeCamIndex].GetComponent<CinemachineVirtualCamera>().Priority = cameraPriority0;
+            cameras[activeCamIndex - 1].GetComponent<CinemachineVirtualCamera>().Priority = cameraPriority1;
+
+            //increase camera index
+            activeCamIndex--;
+
+            //set can change camera to false
+            canChangeCamera = false;
+            canChangeCameraTimer = canChangeCameraTimeLimit;
+        }
+
+        //timer
+        if(!canChangeCamera)
+        {
+            canChangeCameraTimer -= Time.deltaTime;
+
+            if(canChangeCameraTimer < 0)
+            {
+                canChangeCamera = true;
+            }
+        }
+
+    }
+
+    private void TiltBoard()
+    {
+        float cameraX = activeCam.transform.position.x;
+        float cameraZ = activeCam.transform.position.z;
 
         float marbleX = marble.transform.position.x;
         float marbleZ = marble.transform.position.z;
@@ -63,8 +133,8 @@ public class PlayerController : MonoBehaviour
         float diffX = cameraX - marbleX;
         float diffZ = cameraZ - marbleZ;
 
-        float diffXAbs = Mathf.Abs(Mathf.Abs(VC1.transform.position.x) - Mathf.Abs(marble.transform.position.x));
-        float diffZAbs = Mathf.Abs(Mathf.Abs(VC1.transform.position.z) - Mathf.Abs(marble.transform.position.z));
+        float diffXAbs = Mathf.Abs(Mathf.Abs(activeCam.transform.position.x) - Mathf.Abs(marble.transform.position.x));
+        float diffZAbs = Mathf.Abs(Mathf.Abs(activeCam.transform.position.z) - Mathf.Abs(marble.transform.position.z));
 
         //side 1
         if (diffZAbs >= diffXAbs && diffZ > 0)
@@ -99,25 +169,5 @@ public class PlayerController : MonoBehaviour
             board.transform.Rotate(AD * TILTSPEED * Time.deltaTime, 0, 0);
         }
 
-    }
-
-    private GameObject GetNearestSide()
-    {
-        float distance = 0;
-        float smallestDistance = 100f;
-        GameObject output = null;
-
-        foreach(GameObject side in sides)
-        {
-            distance = Vector3.Distance(side.transform.position, VC1.transform.position);
-            if(distance < smallestDistance)
-            {
-                smallestDistance = distance;
-                output = side;
-            }
-        }
-
-        //Debug.Log(output);
-        return output;
     }
 }
