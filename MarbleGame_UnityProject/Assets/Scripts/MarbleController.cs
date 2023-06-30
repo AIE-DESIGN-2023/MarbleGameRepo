8cd5b68e0bb3d88ec1116a2e6c37068d15bd73c6
Ru McPharlin <s220749@students.aie.edu.au>
2023-06-30 15:12:14 +0930

    Rolling and impact codebase done

--- MarbleGame_UnityProject/Assets/Scripts/MarbleController.cs
+++ MarbleGame_UnityProject/Assets/Scripts/MarbleController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MarbleController : MonoBehaviour
@@ -16,9 +17,18 @@ public class MarbleController : MonoBehaviour
     [Space]
     [Space]
     [Header("Rolling")]
+    [SerializeField] bool isGrounded = false;
+    [SerializeField] bool hasGroundImpactPlayed = false;
+    [SerializeField] float groundImpactTimer;
+    [SerializeField] float groundImpactWaitTime;
+    [SerializeField] float rollingAudioDecreaseSpeed;
     [SerializeField] AudioClip rollingWood;
     [Space]
     [Header("Impacts")]
+    [Space]
+    [SerializeField] float softThreshold;
+    [SerializeField] float hardThreshold;
+    [Space]
     [SerializeField] AudioClip tempClip;
     [Space]
     [SerializeField] List<AudioClip> wallWoodImpactsSoft;
@@ -40,30 +50,79 @@ public class MarbleController : MonoBehaviour
     [Header("Marble Variables")]
     [SerializeField] float marbleMagnitude;
     [SerializeField] float maxMagnitude;
+    [SerializeField] float pitchOffset;
+    [SerializeField] float pitchVariation;
+    [SerializeField] float pitchMaxVariation;
+
+    private void Start()
+    {
+        rollingSource.clip = rollingWood;
+    }
 
     //update
     private void Update()
     {
-        //get marble magnitude
-        marbleMagnitude = Mathf.Abs(GetComponent<Rigidbody>().velocity.x) + Mathf.Abs(GetComponent<Rigidbody>().velocity.y) + Mathf.Abs(GetComponent<Rigidbody>().velocity.z);
-        
-        //clamp marble magnitude
-        marbleMagnitude = Mathf.Clamp(marbleMagnitude, 0, maxMagnitude);
+        VolumneCalculator();
 
-        //divide marble magnitude by max
-        marbleMagnitude = marbleMagnitude / maxMagnitude;
+        if (isGrounded)
+        {
+            //apply marble magnitude
+            rollingSource.volume = marbleMagnitude;
+            rollingSource.pitch = pitchVariation+pitchOffset;
 
-        //apply marble magnitude
-        rollingSource.volume = marbleMagnitude;
+            //floor impact
+            if(!hasGroundImpactPlayed)
+            {
+                hasGroundImpactPlayed = true;
+
+                GetWoodImpactClip();
+
+                //set volume
+                impactSource.volume = 1;
+                //play clip
+                impactSource.Play();
+            }
+        }
+        else
+        {
+            rollingSource.volume -= Time.deltaTime * rollingAudioDecreaseSpeed;
+            groundImpactTimer -= Time.deltaTime;
+        }
+
+        if(groundImpactTimer < 0 && !isGrounded)
+        {
+            hasGroundImpactPlayed = false;
+        }
     }
 
-    //impact sounds
+    private void GetWoodImpactClip()
+    {
+        if (marbleMagnitude > hardThreshold)
+        {
+            Debug.Log("PLAY HARD WOOD WALL SOUND");
+            impactSource.clip = wallWoodImpactsHard[Random.Range(0, wallWoodImpactsHard.Count)];
+        }
+        else if (marbleMagnitude > softThreshold && marbleMagnitude < hardThreshold)
+        {
+            Debug.Log("PLAY MEDIUM WOOD WALL SOUND");
+            impactSource.clip = wallWoodImpactsMedium[Random.Range(0, wallWoodImpactsMedium.Count)];
+        }
+        else if (marbleMagnitude < softThreshold)
+        {
+            Debug.Log("Play soft sound");
+            impactSource.clip = wallWoodImpactsSoft[Random.Range(0, wallWoodImpactsSoft.Count)];
+        }
+    }
+
+    //on collision enter
     private void OnCollisionEnter(Collision collision)
     {
         //wood wall
         if(collision.gameObject.tag == WALLWOODTAG)
         {
-            //
+            Debug.Log("WALL WOOD TAG COLLISION ENTER");
+            GetWoodImpactClip();
+            PlayImpact();
         }
 
 
@@ -73,5 +132,45 @@ public class MarbleController : MonoBehaviour
 
         }
 
+        //floor wood
+        if (collision.gameObject.tag == FLOORWOODTAG)
+        {
+            Debug.Log("FLOOR WOOD TAG ENTER");
+            isGrounded = true;
+        }
+    }
+
+    private void PlayImpact()
+    {
+        //set volume
+        impactSource.volume = marbleMagnitude;
+        //play clip
+        impactSource.Play();
+    }
+
+    private void VolumneCalculator()
+    {
+        //get marble magnitude
+        marbleMagnitude = Mathf.Abs(GetComponent<Rigidbody>().velocity.x) + Mathf.Abs(GetComponent<Rigidbody>().velocity.y) + Mathf.Abs(GetComponent<Rigidbody>().velocity.z);
+
+        //clamp marble magnitude
+        marbleMagnitude = Mathf.Clamp(marbleMagnitude, 0, maxMagnitude);
+
+        //divide marble magnitude by max
+        marbleMagnitude = marbleMagnitude / maxMagnitude;
+
+        //calculate pitch variation
+        pitchVariation = (marbleMagnitude - 0.5f);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //wood floor
+        if (collision.gameObject.tag == FLOORWOODTAG)
+        {
+            Debug.Log("FLOOR WOOD TAG EXIT");
+            isGrounded = false;
+            groundImpactTimer = groundImpactWaitTime;
+        }
     }
 }
