9d7eecc9729b60a51dcb5de555b54dabc6ea437f
Ru McPharlin <s220749@students.aie.edu.au>
2023-07-26 16:13:03 +0930

    Fixed up jakes levels

--- MarbleGame_UnityProject/Assets/Scripts/DeathBoxScript.cs
+++ MarbleGame_UnityProject/Assets/Scripts/DeathBoxScript.cs
@@ -30,17 +30,19 @@ public class DeathBoxScript : MonoBehaviour
         playerController.playerActive = false;
 
         yield return new WaitForSeconds(0.55f);
+        //reset board rotation
+        playerController.currentRot = Vector3.zero;
+        playerController.targetRot = Vector3.zero;
+        playerController.board.transform.rotation = Quaternion.identity;
+
+        yield return new WaitForSeconds(0.15f);
+
         //reset marble position
         playerController.marble.transform.position = playerController.spawnPos.transform.position;
         playerController.marble.GetComponent<Rigidbody>().Sleep();
         playerController.marble.GetComponent<Rigidbody>().WakeUp();
 
-        //reset board rotation
-        playerController.currentRot = Vector3.zero;
-        playerController.targetRot = Vector3.zero;
-        playerController.board.transform.rotation = Quaternion.identity;
-        
-
+     
         yield return new WaitForSeconds(0.25f);
 
         playerController.gameController.GetComponent<GameController>().UIAnimator.SetTrigger("Death Enter");
